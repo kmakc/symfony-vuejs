d221bfd208521ace6444c67cb59da2adeb8cd294
kmakc <kmakc@yandex.ru>
2021-09-26 18:14:24 +0300

    form attributes

--- src/Form/EditProductFormType.php
+++ src/Form/EditProductFormType.php
@@ -3,17 +3,35 @@
 namespace App\Form;
 
 use App\Entity\Product;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class EditProductFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('price')
+            ->add('title', TextType::class, [
+                'label' => 'Title (from class)',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([],'Should be not blank')
+                ]
+            ])
+            ->add('price', NumberType::class, [
+                'label' => "Price (from class)",
+                'scale' => 2,
+                'html5' => true,
+                'attr' => [
+                    'step' => 0.1
+                ]
+
+            ])
             ->add('quantity')
             ->add('description')
             ->add('isPublished')
